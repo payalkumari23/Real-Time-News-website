6ce705433ed50d64a77f3b1ba2a81ca30ad647d8
payalkumari23 <payal.kumari.24@aot.edu.in>
2026-05-15 00:06:33 +0530

    fix vite base path

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react() , tailwindcss()],   
+  base: '/Real-Time-News-website/'
   
 })
